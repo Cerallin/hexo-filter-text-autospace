a44c19330d127d529365975c78161a8c253370fe
Cerallin <cerallin@cerallin.top>
2021-12-11 16:08:52 +0800

    opt: Always insert tags nearby hanzi

--- test/filter.test.js
+++ test/filter.test.js
@@ -52,25 +52,25 @@ test('test ignore space', () => {
 test('test cross match 1', () => {
     let str = `<!DOCTYPE html><html><head></head><body>中文<em>English</em></body></html>`;
     expect(filter.process(str))
-        .toBe(`<!DOCTYPE html><html><head></head><body>中文<em><hanla></hanla>English</em></body></html>`)
+        .toBe(`<!DOCTYPE html><html><head></head><body>中文<hanla></hanla><em>English</em></body></html>`)
 })
 
 test('test cross match 2', () => {
     let str = `<!DOCTYPE html><html><head></head><body><em>中文</em>English</body></html>`;
     expect(filter.process(str))
-        .toBe(`<!DOCTYPE html><html><head></head><body><em>中文</em><hanla></hanla>English</body></html>`)
+        .toBe(`<!DOCTYPE html><html><head></head><body><em>中文<hanla></hanla></em>English</body></html>`)
 })
 
 test('test cross match 3', () => {
     let str = `<!DOCTYPE html><html><head></head><body><a href="link">中文</a><em>English</em></body></html>`;
     expect(filter.process(str))
-        .toBe(`<!DOCTYPE html><html><head></head><body><a href="link">中文</a><em><hanla></hanla>English</em></body></html>`)
+        .toBe(`<!DOCTYPE html><html><head></head><body><a href="link">中文<hanla></hanla></a><em>English</em></body></html>`)
 })
 
 test('test cross match complex', () => {
     let str = `<!DOCTYPE html><html><head></head><body><div><div><div><a href="link">中文English中文</a></div></div></div><em>English</em></body></html>`;
     expect(filter.process(str))
-        .toBe(`<!DOCTYPE html><html><head></head><body><div><div><div><a href="link">中文<hanla></hanla>English<hanla></hanla>中文</a></div></div></div><em><hanla></hanla>English</em></body></html>`)
+        .toBe(`<!DOCTYPE html><html><head></head><body><div><div><div><a href="link">中文<hanla></hanla>English<hanla></hanla>中文<hanla></hanla></a></div></div></div><em>English</em></body></html>`)
 })
 
 test('test filter div only', () => {
