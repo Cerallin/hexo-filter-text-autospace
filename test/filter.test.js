54c361e42f4c6337353563b6dc90eff98a0d98be
Cerallin <cerallin@cerallin.top>
2023-01-18 16:41:57 +0800

    chore: Optimize tests & change let -> const
    Signed-off-by: Cerallin <cerallin@cerallin.top>

--- test/filter.test.js
+++ test/filter.test.js
@@ -1,114 +1,218 @@
 const Filter = require("../lib/filter");
 
 const conf = {
-    enable: false,
-    inject_css: true,
-    tag_name: 'hanla',
-    entry: {
-        name: 'body',
-        type: 'tag',
-    }
+  enable: false,
+  inject_css: true,
+  tag_name: 'hanla',
+  entry: {
+    name: 'body',
+    type: 'tag',
+  }
 };
 
 const filter = new Filter(conf);
 
-test('test nothing', () => {
-    let str = `<!DOCTYPE html><html><head></head><body class="hanla other-class"></body></html>`;
-    expect(filter.process(str))
-        .toBe(`<!DOCTYPE html><html><head></head><body class="hanla other-class"></body></html>`)
+function fragment(...content) {
+  return [
+    '<!DOCTYPE html>',
+    '<html>',
+    '<head>',
+    '</head>',
+    '<body>',
+    ...content,
+    '</body>',
+    '</html>'
+  ].join('');
+}
+
+test('test empty', () => {
+  const str = fragment();
+  expect(filter.process(str)).toBe(str)
 })
 
-test('test unchange', () => {
-    let str = `<!DOCTYPE html><html><head></head><body class="hanla other-class">字</body></html>`;
-    expect(filter.process(str))
-        .toBe(`<!DOCTYPE html><html><head></head><body class="hanla other-class">字</body></html>`)
+test('test unchanged', () => {
+  const str = fragment('字');
+  expect(filter.process(str)).toBe(str);
 })
 
 test('test inner text', () => {
-    let str = `<!DOCTYPE html><html><head></head><body class="hanla other-class">中文English中文</body></html>`;
-    expect(filter.process(str))
-        .toBe(`<!DOCTYPE html><html><head></head><body class="hanla other-class">中文<hanla></hanla>English<hanla></hanla>中文</body></html>`)
+  const input = fragment('中文English中文');
+  const output = fragment('中文', '<hanla></hanla>', 'English', '<hanla></hanla>', '中文');
+  expect(filter.process(input)).toBe(output);
 })
 
-
 test('test already inserted', () => {
-    let str = `<!DOCTYPE html><html><head></head><body class="hanla other-class">中文<hanla></hanla>English</body></html>`;
-    expect(filter.process(str))
-        .toBe(`<!DOCTYPE html><html><head></head><body class="hanla other-class">中文<hanla></hanla>English</body></html>`)
+  const str = fragment('中文', '<hanla></hanla>', 'English');
+  expect(filter.process(str)).toBe(str);
 })
 
 test('test wrapping inner text', () => {
-    let str = `<!DOCTYPE html><html><head></head><body class="hanla other-class">中文English中文</body></html>`;
-    expect(filter.process(str))
-        .toBe(`<!DOCTYPE html><html><head></head><body class="hanla other-class">中文<hanla></hanla>English<hanla></hanla>中文</body></html>`)
+  const input = fragment('中文English中文');
+  const output = fragment('中文', '<hanla></hanla>', 'English', '<hanla></hanla>', '中文');
+  expect(filter.process(input)).toBe(output);
 })
 
 test('test ignore space', () => {
-    let str = `<!DOCTYPE html><html><head></head><body class="hanla other-class">中文 English</body></html>`;
-    expect(filter.process(str))
-        .toBe(`<!DOCTYPE html><html><head></head><body class="hanla other-class">中文 English</body></html>`)
+  const str = fragment('中文 English');
+  expect(filter.process(str)).toBe(str);
 })
 
 test('test cross match 1', () => {
-    let str = `<!DOCTYPE html><html><head></head><body>中文<em>English</em></body></html>`;
-    expect(filter.process(str))
-        .toBe(`<!DOCTYPE html><html><head></head><body>中文<hanla></hanla><em>English</em></body></html>`)
+  const input = fragment('中文<em>English</em>');
+  const output = fragment('中文', '<hanla></hanla>', '<em>English</em>');
+  expect(filter.process(input)).toBe(output);
 })
 
 test('test cross match 2', () => {
-    let str = `<!DOCTYPE html><html><head></head><body><em>中文</em>English</body></html>`;
-    expect(filter.process(str))
-        .toBe(`<!DOCTYPE html><html><head></head><body><em>中文<hanla></hanla></em>English</body></html>`)
+  const input = fragment('<em>中文</em>English');
+  const output = fragment('<em>中文<hanla></hanla></em>', 'English');
+  expect(filter.process(input)).toBe(output);
 })
 
 test('test cross match 3', () => {
-    let str = `<!DOCTYPE html><html><head></head><body><a href="link">中文</a><em>English</em></body></html>`;
-    expect(filter.process(str))
-        .toBe(`<!DOCTYPE html><html><head></head><body><a href="link">中文<hanla></hanla></a><em>English</em></body></html>`)
+  const input = fragment('<a href="link">中文</a>', '<em>English</em>');
+  const output = fragment('<a href="link">中文<hanla></hanla>', '</a>', '<em>English</em>')
+  expect(filter.process(input)).toBe(output);
 })
 
 test('test cross match complex', () => {
-    let str = `<!DOCTYPE html><html><head></head><body><div><div><div><a href="link">中文English中文</a></div></div></div><em>English</em></body></html>`;
-    expect(filter.process(str))
-        .toBe(`<!DOCTYPE html><html><head></head><body><div><div><div><a href="link">中文<hanla></hanla>English<hanla></hanla>中文<hanla></hanla></a></div></div></div><em>English</em></body></html>`)
+  const input = fragment(
+    '<div>',
+    '<div>',
+    '<div>',
+    '<a href="link">中文English中文</a>',
+    '</div>',
+    '</div>',
+    '</div>',
+    '<em>English</em>');
+
+  const output = fragment(
+    '<div>',
+    '<div>',
+    '<div>',
+    '<a href="link">',
+    '中文',
+    '<hanla></hanla>',
+    'English',
+    '<hanla></hanla>',
+    '中文',
+    '<hanla></hanla>',
+    '</a>',
+    '</div>',
+    '</div>',
+    '</div>',
+    '<em>English</em>');
+
+  expect(filter.process(input)).toBe(output);
 })
 
 test('test filter div only', () => {
-    conf.entry.name = 'div';
-    let str = `<!DOCTYPE html><html><head></head><body class="hanla other-class"><p>字note</p><div>中文English</div></body></html>`;
-    expect((new Filter(conf)).process(str))
-        .toBe(`<!DOCTYPE html><html><head></head><body class="hanla other-class"><p>字note</p><div>中文<hanla></hanla>English</div></body></html>`)
-    conf.entry.name = 'body';
+  const input = fragment('<p>字note</p>', '<div>中文English</div>');
+  const output = fragment(
+    '<p>字note</p>',
+    '<div>',
+    '中文',
+    '<hanla></hanla>',
+    'English',
+    '</div>');
+
+  expect((new Filter({
+    ...conf,
+    entry: { name: 'div' }
+  })).process(input))
+    .toBe(output);
 })
 
 test('test filter .hanla only', () => {
-    conf.entry.type = "class";
-    conf.entry.name = "hanla";
-    let str = `<!DOCTYPE html><html><head></head><body><p>字note</p><div class="hanla other-class">中文English</div></body></html>`;
-    expect((new Filter(conf)).process(str))
-        .toBe(`<!DOCTYPE html><html><head></head><body><p>字note</p><div class="hanla other-class">中文<hanla></hanla>English</div></body></html>`)
+  const input = fragment(
+    '<p>字note</p>',
+    '<div class="hanla other-class">',
+    '中文English',
+    '</div>');
+  const output = fragment(
+    '<p>字note</p>',
+    '<div class="hanla other-class">',
+    '中文',
+    '<hanla></hanla>',
+    'English',
+    '</div>');
 
-    conf.entry.type = "body";
-    conf.entry.name = "tag";
+  expect((new Filter({
+    ...conf,
+    entry: {
+      type: 'class',
+      name: 'hanla'
+    }
+  })).process(input))
+    .toBe(output);
 })
 
 test('test match multi nodes', () => {
-    conf.entry.type = "class";
-    conf.entry.name = "hanla";
-    let str = `<!DOCTYPE html><html><head></head><body><div class="hanla">English中文</div><div class="hanla">中文English<div class="hanla">中文English</div></div></body></html>`;
-    expect(filter.process(str))
-        .toBe(`<!DOCTYPE html><html><head></head><body><div class="hanla">English<hanla></hanla>中文</div><div class="hanla">中文<hanla></hanla>English<div class="hanla"><hanla></hanla>中文<hanla></hanla>English</div></div></body></html>`)
-    conf.entry.type = "body";
-    conf.entry.name = "tag";
+  const input = fragment(
+    '<div class="hanla">English中文</div>',
+    '<div class="hanla">',
+    '中文English',
+    '<div class="hanla">',
+    '中文English',
+    '</div>',
+    '</div>');
+  const output = fragment(
+    '<div class="hanla">',
+    'English',
+    '<hanla></hanla>',
+    '中文',
+    '</div>',
+    '<div class="hanla">',
+    '中文',
+    '<hanla></hanla>',
+    'English',
+    '<div class="hanla">',
+    '<hanla></hanla>',
+    '中文',
+    '<hanla></hanla>',
+    'English',
+    '</div>',
+    '</div>');
+
+  expect(filter.process(input)).toBe(output);
 })
 
 
 test('test bug', () => {
-    conf.entry.type = "class";
-    conf.entry.name = "hanla";
-    let str = `<!DOCTYPE html><html><head></head><body><div class="hanla"><p>正如<a href="#系统要求">系统要求</a>中parallel的输出中所提到的，当论文数据处理用到了该工具的时候，应该按照parallel的作者给出的形式<span class="citation" data-cites="tange2011gnu">[<a href="#ref-tange2011gnu" role="doc-biblioref">1</a>]</span>进行引用。</p></div></body></html>`;
-    expect(filter.process(str))
-        .toBe(`<!DOCTYPE html><html><head></head><body><div class="hanla"><p>正如<a href="#系统要求">系统要求</a>中<hanla></hanla>parallel<hanla></hanla>的输出中所提到的，当论文数据处理用到了该工具的时候，应该按照<hanla></hanla>parallel<hanla></hanla>的作者给出的形式<hanla></hanla><span class="citation" data-cites="tange2011gnu">[<a href="#ref-tange2011gnu" role="doc-biblioref">1</a>]</span><hanla></hanla>进行引用。</p></div></body></html>`)
-    conf.entry.type = "body";
-    conf.entry.name = "tag";
+  const input = fragment(
+    '<div class="hanla">',
+    '<p>',
+    '正如',
+    '<a href="#系统要求">系统要求</a>',
+    '中parallel的输出中所提到的，当论文数据处理用到了该工具的时候，应该按照parallel的作者给出的形式',
+    '<span class="citation" data-cites="tange2011gnu">',
+    '[<a href="#ref-tange2011gnu" role="doc-biblioref">1</a>]',
+    '</span>',
+    '进行引用。',
+    '</p>',
+    '</div>');
+  const output = fragment(
+    '<div class="hanla">',
+    '<p>',
+    '正如',
+    '<a href="#系统要求">系统要求</a>',
+    '中',
+    '<hanla></hanla>',
+    'parallel',
+    '<hanla></hanla>',
+    '的输出中所提到的，当论文数据处理用到了该工具的时候，应该按照',
+    '<hanla></hanla>',
+    'parallel',
+    '<hanla></hanla>',
+    '的作者给出的形式',
+    '<hanla></hanla>',
+    '<span class="citation" data-cites="tange2011gnu">',
+    '[<a href="#ref-tange2011gnu" role="doc-biblioref">1</a>]',
+    '</span>',
+    '<hanla></hanla>',
+    '进行引用。',
+    '</p>',
+    '</div>')
+  expect(filter.process(input))
+    .toBe(output);
 })
